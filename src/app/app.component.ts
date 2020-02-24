6cef536c509e60a55a645b3fef112d609f6aa22f
Nate Bedortha <nbedortha@esri.com>
2020-02-24 13:21:35 -0800

    Add esri-map component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'vrt-demo';
+  title = "vrt-demo";
+
+  // Set our map properties
+  mapCenter = [-122.4194, 37.7749];
+  basemapType = "satellite";
+  mapZoomLevel = 12;
+
+  // See app.component.html
+  mapLoadedEvent(status: boolean) {
+    console.log("The map loaded: " + status);
+  }
 }
